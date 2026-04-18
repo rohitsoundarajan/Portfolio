245b8725fca6358b2bd13ee7b908432e3af42887
rohitsoundarajan <rohitsoundarajan@gmail.com>
2026-04-18 16:42:25 +0530

    Fix basepath

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',      // This tells Next.js to generate a static HTML site
-  images: {
-    unoptimized: true,   // GitHub doesn't support Next.js's dynamic image server
-  },
-  // IMPORTANT: If your repository is NOT named 'rohitsoundarajan.github.io'
-  // and is instead something like 'portfolio', uncomment the line below:
-  // basePath: '/portfolio', 
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/Portfolio', // Add this line
 };
 
 export default nextConfig;
